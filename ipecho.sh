f567fe3c6a31eac47bcf05bdb1db806e9d6ba0ff
Morryk <32412176+Morryk@users.noreply.github.com>
2024-09-05 14:56:29 +0200

    Update ipecho.sh

--- ipecho.sh
+++ ipecho.sh
@@ -9,7 +9,7 @@ then
         while true
         do
                 sleep $1s
-                data=$(date +"%D %T - ")
+                data=$(date +"%d/%m/%y %T - ")
                 ip=$(curl https://ipecho.net/plain -s)
                 echo $data$ip
         done
